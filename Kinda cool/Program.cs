721158adf29dc6f951e0347626d1792b69c2fa96
marcus <marcus@DESKTOP-SELO19E>
2019-11-04 12:58:46 +0100

    Add project files.

---
+++ Kinda cool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Kinda_cool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
